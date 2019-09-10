4dde96092c6b4a3b27c38bce6880609636e16f5f
toshioxgnu <jozho.g@gmail.com>
2019-09-10 13:55:43 -0300

    Posible arreglo de la funcion Mediano

--- Mediano.py
+++ Mediano.py
@@ -1,7 +1,7 @@
 #Funcion encargada de buscar los marcadores con el criterio
-#MEDIANO de seleccion 
+#MEDIANO de seleccion
 import Busca
-import csv  
+import csv
 import math
 
 def mediano():
@@ -13,33 +13,33 @@ def mediano():
                 if (float(line[2])-float(line1[2])<=1):
                     #SI
                     if (float(line[3])-float(line1[3])<=1) :
-                        if(float(line[3])-float(line1[3])<=2):
+                        if(float(line[3])-float(line1[3])==2):
                             linea=("1"+"\t"+str(line[2])+"\t"+str(line[3])+"\t"+str(line1[2])+"\t"+str(line1[3])+"\t"+"optimo"+"\n")
                             f.write(linea)
-                           
-                    #NO    
-                    else:
+                        
+                    #NO
+                    if(float(line[3])-float(line1[3])>2):
                         linea=("1"+"\t"+str(line[2])+"\t"+str(line[3])+"\t"+str(line1[2])+"\t"+str(line1[3])+"\t"+"NO"+"\n")
                         f.write(linea)
-                      
-                if (float(line[2])-float(line1[2])>=1):
-                    #SI                   
-                    if(float(line1[3])-float(line[3])<=1):
-                        if(float(line[3]-float(line1[3])>=2): 
-                            linea=("1"+"\t"+str(line[2])+"\t"+str(line[3])+"\t"+str(line1[2])+"\t"+str(line1[3])+"\t"+"optimo"+"\n")
-                            f.write(linea)
-                    #NO    
-                    else:
-                        linea=("1"+"\t"+str(line[2])+"\t"+str(line[3])+"\t"+str(line1[2])+"\t"+str(line1[3])+"\t"+"NO"+"\n")
-                        f.write(linea)
-                        break
-
-                                                
-                                  
-
                     
+
+                if (float(line[2])-float(line1[2])==1):
+                    #SI
+                    if(float(line1[3])-float(line[3])<=1):
+                        if(float(line[3])-float(line1[3])>=2):
+                            linea=("1"+"\t"+str(line[2])+"\t"+str(line[3])+"\t"+str(line1[2])+"\t"+str(line1[3])+"\t"+"optimo"+"\n")
+                            f.write(linea)
+                    #NO
+                    else:
+                        linea=("1"+"\t"+str(line[2])+"\t"+str(line[3])+"\t"+str(line1[2])+"\t"+str(line1[3])+"\t"+"NO"+"\n")
+                        f.write(linea)
+                   
+
+
+
+
+
     with open('marcadores_seleccionados.txt','r') as f:
         print("Se encontraron ",ct," marcadores")
         for line in f:
             print (line)
-    
